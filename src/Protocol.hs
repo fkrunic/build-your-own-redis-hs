09b0f391efa8472c014b0e544b99a6f0b66202b1
filipkrunic <fkrunic9@gmail.com>
2023-05-02 12:43:50 -0700

    response data

--- src/Protocol.hs
+++ src/Protocol.hs
@@ -12,10 +12,23 @@ data Message
   | Delete { key :: Text }
   deriving (Generic, Eq, Show)
 
+data Response
+  = Ok
+  | Error
+  | KeyDoesNotExist { badKey :: Text }
+  deriving (Generic, Eq, Show)
+
 instance Binary Message
+instance Binary Response
 
 decodeMessage :: BS.ByteString -> Message
 decodeMessage = decode . BSL.fromStrict
 
 encodeMessage :: Message -> BS.ByteString
 encodeMessage = BS.concat . BSL.toChunks . encode
+
+decodeResponse :: BS.ByteString -> Response
+decodeResponse = decode . BSL.fromStrict
+
+encodeResponse :: Response -> BS.ByteString
+encodeResponse = BS.concat . BSL.toChunks . encode
