936f50be405779cf4dadcd928ef00f3d89de2839
filipkrunic <fkrunic9@gmail.com>
2023-05-02 10:11:57 -0700

    server socket loop

--- app/Client.hs
+++ app/Client.hs
@@ -6,14 +6,20 @@ import Network.Simple.TCP
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.IO as TIO
 
+sendMessage :: Socket -> IO ()
+sendMessage conn = do
+  send conn $ T.encodeUtf8 "hello!"
+  r <- recv conn 100
+  case r of 
+    Nothing -> TIO.putStrLn "Server sent nothing."
+    Just msgBytes -> do 
+      let msg = T.decodeUtf8 msgBytes
+      TIO.putStrLn $ "Server says: " <> msg  
+
 main :: IO ()
 main = do
   TIO.putStrLn "Client started."
   connect "0.0.0.0" "1234" $ \(conn, _) -> do
-    send conn $ T.encodeUtf8 "hello!"
-    r <- recv conn 100
-    case r of 
-      Nothing -> TIO.putStrLn "Server sent nothing."
-      Just msgBytes -> do 
-        let msg = T.decodeUtf8 msgBytes
-        TIO.putStrLn $ "Server says: " <> msg
+    sendMessage conn
+    sendMessage conn
+    sendMessage conn
--- app/Server.hs
+++ app/Server.hs
@@ -2,18 +2,23 @@
 
 module Main where
 
-import Network.Simple.TCP
+import Control.Monad (forever)
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.IO as TIO
+import Network.Simple.TCP
+
+handleConnection :: Socket -> IO ()
+handleConnection conn = forever $ do
+  r <- recv conn 100
+  case r of 
+    Nothing -> pure ()
+    Just msgBytes -> do 
+      let msg = T.decodeUtf8 msgBytes
+      TIO.putStrLn $ "Client says: " <> msg
+      send conn $ T.encodeUtf8 "world!"
 
 main :: IO ()
 main = do 
   TIO.putStrLn "Server started."
-  serve (Host "0.0.0.0") "1234" $ \(socket, _) -> do
-    r <- recv socket 100
-    case r of 
-      Nothing -> TIO.putStrLn "Client sent nothing."
-      Just msgBytes -> do 
-        let msg = T.decodeUtf8 msgBytes
-        TIO.putStrLn $ "Client says: " <> msg
-        send socket $ T.encodeUtf8 "world!"
+  serve (Host "0.0.0.0") "1234" $ \(socket, _) -> 
+    handleConnection socket
