5f320b4eec69b123f7f8ee21d9456c7045faafbc
filipkrunic <fkrunic9@gmail.com>
2023-05-02 12:36:13 -0700

    encoding and decoding messages

--- src/Protocol.hs
+++ src/Protocol.hs
@@ -1,5 +1,7 @@
 module Protocol where
 
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString as BS
 import Data.Binary
 import Data.Text
 import GHC.Generics
@@ -8,6 +10,12 @@ data Message
   = Set { key :: Text, value :: Text }
   | Get { key :: Text }
   | Delete { key :: Text }
-  deriving (Generic, Eq)
+  deriving (Generic, Eq, Show)
 
 instance Binary Message
+
+decodeMessage :: BS.ByteString -> Message
+decodeMessage = decode . BSL.fromStrict
+
+encodeMessage :: Message -> BS.ByteString
+encodeMessage = BS.concat . BSL.toChunks . encode
