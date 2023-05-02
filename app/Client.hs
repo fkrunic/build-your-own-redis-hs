36346ab9c6e4131d9edf693b22efd7055f595dc8
filipkrunic <fkrunic9@gmail.com>
2023-05-02 09:50:39 -0700

    working server / client connection

--- app/Client.hs
+++ app/Client.hs
@@ -1,1 +1,19 @@
-module Client where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Network.Simple.TCP 
+import qualified Data.Text.Encoding as T
+import qualified Data.Text.IO as TIO
+
+main :: IO ()
+main = do
+  TIO.putStrLn "Client started."
+  connect "0.0.0.0" "1234" $ \(conn, _) -> do
+    send conn $ T.encodeUtf8 "hello!"
+    r <- recv conn 100
+    case r of 
+      Nothing -> TIO.putStrLn "Server sent nothing."
+      Just msgBytes -> do 
+        let msg = T.decodeUtf8 msgBytes
+        TIO.putStrLn $ "Server says: " <> msg
--- app/Server.hs
+++ app/Server.hs
@@ -7,12 +7,13 @@ import qualified Data.Text.Encoding as T
 import qualified Data.Text.IO as TIO
 
 main :: IO ()
-main = serve (Host "0.0.0.0") "1234" $ \(socket, _) -> do
-  r <- recv socket 100
+main = do 
   TIO.putStrLn "Server started."
-  case r of 
-    Nothing -> TIO.putStrLn "Client sent nothing."
-    Just msgBytes -> do 
-      let msg = T.decodeUtf8 msgBytes
-      TIO.putStrLn $ "Client sent: " <> msg
-      send socket $ T.encodeUtf8 "world!"
+  serve (Host "0.0.0.0") "1234" $ \(socket, _) -> do
+    r <- recv socket 100
+    case r of 
+      Nothing -> TIO.putStrLn "Client sent nothing."
+      Just msgBytes -> do 
+        let msg = T.decodeUtf8 msgBytes
+        TIO.putStrLn $ "Client says: " <> msg
+        send socket $ T.encodeUtf8 "world!"
